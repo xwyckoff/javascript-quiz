f61c0b6179815b3942cc40d39ede7846c8c497c5
Xander <45044697+xwyckoff@users.noreply.github.com>
2023-02-22 00:04:06 -0500

    added the scoring system and high score page, cleaned up some redundant code

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,12 +2,19 @@ let questionText = $('#questionText')
 let timer = $("#timer");
 let ansOptions = $("#questions");
 let startButton = $("#startButton");
+let questionContainer = $("#questionContainer");
+let scoreList = $("#scoreList");
 let timeLeft = 20;
 let score = 0;
+let getHighScore = JSON.parse(localStorage.getItem('highScores') || "[]");
+let submitScore = $("<button></button>");
+let submitName = $("<input>");
+let timeout;
 
 let quizList = [];
 let currentQuestion;
 
+console.log(getHighScore);
 //define a question class that takes the main question text, 3 incorrect answers, and the correct answer
 class Question {
     constructor(question, opt1, opt2, opt3, correctAns) {
@@ -22,7 +29,7 @@ class Question {
 
     //method to display all the answer options
     setAnswerOpts() {
-        //first clear all the list items from the list of answers
+        //first clear all the list items from the list of answers and clear the previous event listener
         ansOptions.empty();
         ansOptions.off();
         //randomly shuffles the array of answer options around so they aren't always in the same order (explanation written for future reference)
@@ -34,8 +41,10 @@ class Question {
         //creates a list element for each answer option
         this.options.forEach(ans => {
             let answerEl = $('<li>');
-            answerEl.text(ans);
-            answerEl.addClass("optionButton");
+            let answerElButton = $('<button>')
+            answerElButton.text(ans);
+            answerElButton.addClass("btn");
+            answerEl.append(answerElButton);
             ansOptions.append(answerEl);
         });
     }
@@ -48,36 +57,43 @@ quizList = [test, test2];
 
 function startQuiz() {
     //start the timer for the quiz, set the current question to the first question, and remove the starter text
-
     $('#starterText').empty();
     currentQuestion = 0;
     askQuestion(currentQuestion);
-    let timeout = setInterval(() => {
+
+    timeout = setInterval(() => {
         timer.text("Time: " + timeLeft);
         timeLeft--;
-
-        if(timeLeft <= 0){
+    
+        if(timeLeft == 0){
             timer.empty();
-            clearInterval(timeout);
-            questionText.empty();
-            questionText.text("All done!")
-            ansOptions.empty();
-            startButton.remove();
-            $('#starterText').text("Your final score is " + score);
+            endQuiz();
         }
-    }, 1000)
+    }, 1000);
 }
 
 //function to end the quiz, displays high score and gives option to save high score
 function endQuiz() {
     //removing the current question text and adding the "all done" screen
     score = timeLeft;
-    timeLeft = 0;
+    timer.empty();
+    clearInterval(timeout);
     questionText.empty();
-    questionText.text("All done!")
+    questionText.text("All done!");
     ansOptions.empty();
     startButton.remove();
     $('#starterText').text("Your final score is " + score);
+
+    //create input text box
+    submitName.attr("type","text");
+    submitName.attr("id","nameInput");
+    questionContainer.append(submitName);
+
+    //create submit score button
+    submitScore.text("Submit");
+    submitScore.addClass("btn");
+    submitScore.attr("onclick","sendScore();")
+    questionContainer.append(submitScore);
 }
 
 //function to display the question and what to do when the user selects a question
@@ -86,7 +102,7 @@ function askQuestion(questNum) {
     quizList[questNum].setQuestionText();
     quizList[questNum].setAnswerOpts();
     //add event listener for when the list elements are clicked
-    ansOptions.on("click", "li", event => {
+    ansOptions.on("click", "button", event => {
         console.log(questNum);
         //sets the clicked element equal to a jquery object, also increments to next question
         selected = $(event.target);
@@ -94,7 +110,7 @@ function askQuestion(questNum) {
 
         //checks if the selected answer is incorrect, if so it decreases the time by 10 seconds
         if(selected.text() != quizList[questNum].correctAns){
-            timeLeft -= 10;
+            timeLeft > 10 ? timeLeft -= 10 : timeLeft = 0;
             console.log(questNum);
             console.log(selected.text() + " and " + JSON.stringify(quizList[questNum]));
         }
@@ -108,5 +124,30 @@ function askQuestion(questNum) {
         
     });
 }
+//function to send the high score to localStorage
+function sendScore() {
+    let highScore = {
+        "name": submitName.val(),
+        "highScore": score
+    }
+    getHighScore.push(highScore);
+    localStorage.setItem('highScores', JSON.stringify(getHighScore));
+    window.location.href = "highscores.html";
+}
 
-startQuiz();
+//function to clear all the high scores from localStorage
+function clearScores() {
+    localStorage.removeItem('highScores');
+    scoreList.empty();
+}
+
+//function to display the list of scores on the high score page
+function displayScores() {
+    getHighScore.forEach((item) => {
+        let scoreEl = $('<li>');
+        scoreEl.text(item.name + " - " + item.highScore);
+        scoreList.append(scoreEl);
+    });
+}
+
+//startQuiz();
